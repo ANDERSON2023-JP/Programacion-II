18feb2bd5ce86e448ac50f062aed6e2f8e1af412
ANDERSON MANUEL APAZA HUANCA <andersonapaza021@gmail.com>
2025-09-21 23:15:56 -0400

    Add files via upload

---
+++ PRACTICA 3/Aplicacion/src/main/java/JuegoAdivinaImpar.java
@@ -0,0 +1,19 @@
+public class JuegoAdivinaImpar extends JuegoAdivinaNumero {
+
+    public JuegoAdivinaImpar(int numeroDeVidas) {
+        super(numeroDeVidas);
+    }
+
+    @Override
+    public boolean validaNumero(int numero) {
+        if (numero >= 0 && numero <= 10) {
+            if (numero % 2 != 0) {
+                return true;
+            } else {
+                System.out.println("solo se aceptan números IMPARES entre 0 y 10.");
+                return false;
+            }
+        }
+        return false;
+    }
+}
